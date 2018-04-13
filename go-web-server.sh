41398f623fb6836a2b70d570d75440dfc4b6c1d3
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2018-04-13 13:31:58 +0900

    new mimiaka mongo url

--- go-web-server.sh
+++ go-web-server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 5000
-export MIMIAKA_MONGO_URL=mongodb://mimiaka:mimiaka1846@ds023668-a0.mlab.com:23668,ds023668-a1.mlab.com:23668/mimiaka?replicaSet=rs-ds023668
+export MIMIAKA_MONGO_URL=mongodb+srv://mimiaka:mimiaka1846@mimiaka-family-uthsq.mongodb.net/mimiaka
 sudo apt install gnugo
 cd ~/go-web-server
 nohup python3 web.py &
